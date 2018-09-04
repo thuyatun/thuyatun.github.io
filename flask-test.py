751c10491af552d2283321ca5b9c274993c9a020
thuyatun <tunthura97@gmail.com>
2018-09-04 15:59:40 +0630

    add flask test python

--- flask-test.py
+++ flask-test.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -8,3 +8,4 @@ def hello():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
